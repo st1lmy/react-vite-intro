5c1c4dedf4555491930b6b1952bddbe87d2c0fcd
OLzhas Akhmetzhan <olzhas228335@gmail.com>
2025-05-16 23:26:31 +0500

    deploy44

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,7 @@ function App() {
       <main>
         <section>
           <ul>
-            <WayToTeach
-              title={ways[0].title}
-              description={ways[0].description}
-            />
-            <WayToTeach {...ways[1]} />
-            <WayToTeach {...ways[2]} />
-            <WayToTeach {...ways[3]} />
+
           </ul>
         </section>
       </main>
